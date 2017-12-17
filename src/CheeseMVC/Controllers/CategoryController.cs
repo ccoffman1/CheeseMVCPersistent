6ea7f8eafbba486a3ecc3fe152037ea7bedf4218
Gail Fike <gfike19@gmail.com>
2017-12-17 10:41:42 -0600

    finished part 1

--- src/CheeseMVC/Controllers/CategoryController.cs
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CheeseMVC.Data;
 using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CheeseMVC.Controllers
@@ -20,6 +21,32 @@ namespace CheeseMVC.Controllers
         {
             List<CheeseCategory> cheeseCategories = context.Categories.ToList();
             return View(cheeseCategories);
+        }
+
+        public IActionResult Add ()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new cheese to my existing cheeses
+                CheeseCategory newCheeseCategory = new CheeseCategory
+                {
+                    Name = addCategoryViewModel.Name
+                };
+
+                context.Categories.Add(newCheeseCategory);
+                context.SaveChanges();
+
+                return Redirect("/Category");
+            }
+
+            return View(addCategoryViewModel);
         }
     }
 }
